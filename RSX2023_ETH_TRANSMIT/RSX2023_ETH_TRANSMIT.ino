e2cf62b90b881cd3dade54796e334b37b9f79fb3
Andrew Nowakowski <64051143+andrewmnowakowski@users.noreply.github.com>
2022-10-28 14:10:27 -0700

    updated documentation for ethernet.

--- RSX2023_ETH_TRANSMIT/RSX2023_ETH_TRANSMIT.ino
+++ RSX2023_ETH_TRANSMIT/RSX2023_ETH_TRANSMIT.ino
@@ -5,18 +5,21 @@ const bool DEBUG = true;
 // Ethernet Config
 #include "RSX_CH9121_ETH.h"
 
-
 // Main Function
-void setup() {
-  if (DEBUG) Serial.begin(BAUD_RATE);
+void setup()
+{
+  if (DEBUG)
+    Serial.begin(BAUD_RATE);
   eth_begin(DEBUG);
 }
 
-void loop() {
+void loop()
+{
   String message = "Bruhhhhhhh";
   eth_send_message(DEBUG, message);
 
-  if (DEBUG) Serial.println("Message: " + eth_receive_message(DEBUG));
+  if (DEBUG)
+    Serial.println("Message: " + eth_receive_message(DEBUG));
   delay(100);
   eth_debug();
 }
--- RSX2023_ETH_TRANSMIT/RSX_CH9121_ETH.h
+++ RSX2023_ETH_TRANSMIT/RSX_CH9121_ETH.h
@@ -3,9 +3,14 @@
 
 #include "Arduino.h"
 
+/*
+The ethernet chip used in this application is the "Network and UART Transparent Transmission Chip CH9121"
+Datasheet: https://www.waveshare.com/w/upload/a/a9/CH9121DS1.PDF or in GitHub repo
+Commands: https://www.waveshare.com/w/upload/e/ef/CH9121_SPCC.pdf or in GitHub repo
+*/
 
 void eth_begin(bool debug);
-void eth_send_message(bool debug, String& message);
+void eth_send_message(bool debug, String &message);
 String eth_receive_message(bool debug);
 int eth_available();
 int eth_availableForWrite();
